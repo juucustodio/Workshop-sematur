574d2e4fcf2380c36f74bd3642981d0362b833bd
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-10-06 12:11:17 -0300

    Implements the project

---
+++ AppDemo/AppDemo/AppDemo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular(object sender, EventArgs e)
+        {
+            int NumeroA = int.Parse(TextoA.Text);
+            int NumeroB = int.Parse(TextoB.Text);
+            int Total = NumeroA + NumeroB;
+
+            Resultado.Text = "Total = "+Total;
+        }
+    }
+}
